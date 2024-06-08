4f9d64391e745d06d8db6382146fd84277f4185f
pratiksha <rautpratiksha0802@gmail.com>
2024-06-08 18:30:27 +0530

    lowerCase clear copy text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 function App() {
   return (
     <>
       <Navbar title="textUtils" aboutText="About us"/>
-      <div className='container' my="3">
+      <div className='container my-3'>
       <TextForm heading="enter the text to Analyze below"/>
       </div>
+      <About/>
       {/* <Navbar/> */}
       </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,23 +1,51 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-    const [text, setText]=useState("Enter Text Here");
+    const [text, setText]=useState("");
     const handleUpClick=()=>
     {
         let newText=text.toUpperCase();
         setText(newText);
     }
+    const handleDownClick=()=>
+    {
+        let newText=text.toLowerCase();
+        setText(newText);
+    }
+    const handleClearClick=()=>{
+        setText('');
+    }
+    const handleCopyClick=()=>
+    {
+       var text=document.getElementById('myBox') 
+       text.select();
+       navigator.clipboard.writeText(text.value);
+    }
     const handleOnChange=(event)=>{
         setText(event.target.value);
     }
 
+
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
       <div className='mb-3'>
         <textarea className='form-control' value={text} onChange={handleOnChange} id='myBox' rows='8'></textarea>
       </div>
-      <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className='btn btn-primary mx-2' onClick={handleDownClick}>Convert to LowerCase</button>
+      <button className='btn btn-primary mx-2' onClick={handleClearClick}>Clear Text</button>
+      <button className='btn btn-primary mx-2' onClick={handleCopyClick}>Copy Text</button>
+    
     </div>
+    <div className='container my-3'>
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008*text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
