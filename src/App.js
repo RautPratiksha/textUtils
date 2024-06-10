1bfe318908ace94bbfce3b699d6588d21b264357
pratiksha <rautpratiksha0802@gmail.com>
2024-06-10 14:28:31 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import { useState } from 'react';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   const [mode, setMode]=useState('light');
   const toggleMode=()=>
@@ -10,20 +15,31 @@ function App() {
     if(mode==='light'){
       setMode("dark")
       document.body.style.backgroundColor='#042743';
+      //document.title='textUtils-darkMode'//title change
     }
     else{
       setMode('light')
       document.body.style.backgroundColor='white';
+      //document.title='textUtils-lightMode'//title change
     }
     
   }
   return (
     <>
+    <Router>
       <Navbar title="textUtils" mode={mode} toggleMode={toggleMode}/>
       <div className='container my-3'>
-      <TextForm heading="enter the text to Analyze below" mode={mode}/>
+      <Routes>
+        <Route path="/about" element={<About/>}>
+        </Route>
+        <Route path="/" element={<TextForm heading="enter the text to Analyze below" mode={mode}/>}>
+        
+        </Route>
+      </Routes>
       </div>
-      <About/>
+      </Router>
+      
+    
       </>
   );
 }
